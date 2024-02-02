ed770189ed976004248d5454fb2d100645d70bbf
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2024-02-02 16:47:31 -0300

    Initialized project. Added necessary images.

---
+++ claimer.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+from time import sleep
+import numpy as np
+from subprocess import Popen
+from PIL import ImageGrab
+import pyautogui as pag
+import cv2
+
+# https://github.com/MasonStooksbury/Free-Games-V2/blob/main/Free_Games_V2.py
+
+print("all modules loaded")
