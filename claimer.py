ddf004d8defc8f0badf7e80dea9b6ff0090336e4
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2024-02-03 10:14:24 -0300

    Add a logger. Add verbose mode to expose heroic output. Simple test to open and go to store page.

--- claimer.py
+++ claimer.py
@@ -1,21 +1,37 @@
 from pathlib import Path
 from time import sleep
 import numpy as np
-from subprocess import Popen
+from subprocess import Popen, PIPE
 from PIL import ImageGrab
 import pyautogui as pag
 import cv2
+import sys
+import atexit
+from datetime import datetime
+from inspect import currentframe, getframeinfo
 
 # https://github.com/MasonStooksbury/Free-Games-V2/blob/main/Free_Games_V2.py
+
+
+# Utils
+def logger(line_number, message):
+    print(f"[{str(datetime.now())}][LN:{line_number:4}]: {message}")
+
+def exit_with_error(error, line_number):
+    logger(line_number, f"ERROR:\n\t{error}\n")
+    logger(line_number, "Exiting...")
+    sys.exit()
 
 
 # Function to easily capture screenshots
 def captureScreenshot():
     # Capture the entire screen, convert to a numpy array, then convert to OpenCV format
+    logger(getframeinfo(currentframe()).lineno, "Taking screenshot...")
     return cv2.cvtColor(np.array(ImageGrab.grab()), cv2.COLOR_RGB2BGR)
 
 
 def findTemplateInScreenshot(screenshot, template_path):
+    logger(getframeinfo(currentframe()).lineno, "Looking for template in screenshot...")
     # Read the template image
     template = cv2.imread(template_path, 0)
     template_w, template_h = template.shape[::-1]
@@ -39,6 +55,44 @@ def findTemplateInScreenshot(screenshot, template_path):
         # Draw a green rectangle around the matched area
         # cv2.rectangle(screenshot, top_left, bottom_right, (0, 255, 0), 2)
 
+        logger(getframeinfo(currentframe()).lineno, "Found.")
         return screenshot, (center_x, center_y)
     else:
+        logger(getframeinfo(currentframe()).lineno, "Not Found.")
         return None, None
+
+
+verbose = True if len(sys.argv) > 1 and (sys.argv[1] == '-v' or sys.argv[1] == '--verbose') else False
+
+cmd_to_execute = ["heroic"]
+out_dest = None if verbose else PIPE
+
+# Open the Heroic Games Launcher Desktop App
+logger(getframeinfo(currentframe()).lineno, f"Executing command: '{str(cmd_to_execute)}'...")
+process = Popen(cmd_to_execute, stdout=out_dest, stderr=out_dest)
+
+atexit.register(process.terminate)
+
+# Give the app a second to startup
+sleep(5)
+
+# Looking for the "Store" link on the left side of the app
+logger(getframeinfo(currentframe()).lineno, "Looking for store button in menu...")
+matched_image, coords = findTemplateInScreenshot(
+    captureScreenshot(), str(Path("templates").joinpath("store_menu_button.png"))
+)
+
+# If not found then app didn't open
+if matched_image is None:
+    exit_with_error(
+        "Couldn't find store button, check if Heroic Game Launcher actually opens.",
+        getframeinfo(currentframe()).lineno,
+    )
+# Otherwise, make sure the window is focused, then get our free game
+else:
+    pag.click(x=coords[0], y=coords[1])
+    sleep(2)
+
+# For now just open, go to store and close
+logger(getframeinfo(currentframe()).lineno, "Finished. Exiting...")
+sys.exit()
